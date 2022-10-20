0fd897ad04f3fd9b3d6a47e18e9c5219cbae1a3b
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2022-10-20 14:20:02 +0200

    Commit for cluster

--- scripts/speech/pretrain_features.py
+++ scripts/speech/pretrain_features.py
@@ -22,9 +22,12 @@ if __name__ == "__main__":
     stacked_features = [dict() for i in range(n_averaging)]
     seeds_data, seeds_nys, _, seeds_cv = expe_funcs.draw_seeds(
         n_averaging, config.SEED)
+    base_path = str(exec_path.parent.parent)
+    out_folder = base_path + "/outputs/pretraining/"
+    expe_funcs.create_folder(out_folder)
     for i in range(n_averaging):
         Xtrain, Ytrain_full_ext, Ytrain_full, Xtest, Ytest_full_ext, Ytest_full = expe_funcs.load_speech_dataset(
-            seeds_data[i], str(exec_path.parent.parent) + "/datasets/dataspeech/raw/", n_train=config.N_TRAIN)
+            seeds_data[i], base_path + "/datasets/dataspeech/raw/", n_train=config.N_TRAIN)
         Xtrain = torch.Tensor(Xtrain)
         for n_feat in n_feats:
             # Ks, feats = expe_funcs.pretrain_nystrom_features(Xtrain, n_feat, SpeechKernel, torch.logspace(
@@ -33,5 +36,5 @@ if __name__ == "__main__":
                 Xtrain, n_feat, SpeechKernel, torch.logspace *(config.KERNEL_INPUT_GAMMA), seeds_cv[i], seeds_nys[i], n_splits=config.CV_SPLIT)
             stacked_features[i][n_feat] = (Ks, feats)
             print(n_feat)
-    with open(str(exec_path.parent.parent) + "/outputs/pretraining/features_speech.pkl", "wb") as outp:
+    with open(base_path + "/outputs/pretraining/features_speech.pkl", "wb") as outp:
         pickle.dump(stacked_features, outp)
