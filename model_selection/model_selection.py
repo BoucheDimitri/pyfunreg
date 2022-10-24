0246c00fdd4c88bc5d676bfda528d39088ecd2c7
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2022-10-24 13:56:14 +0200

    New exec test

--- model_selection/model_selection.py
+++ model_selection/model_selection.py
@@ -97,11 +97,11 @@ def cv_features(esti, features, X, Y, Ks=None, Yeval=None, phis=None, n_splits=5
 def tune_consecutive(estis, losses, X, Y, K=None, 
                      Yeval=None, n_splits=5, reduce_stat="median", 
                      random_state=342, n_jobs=-1):
-    # with parallel_backend("loky"):
-    #     mses = Parallel(n_jobs=n_jobs)(
-    #         delayed(cv_consecutive)(esti, losses, X, Y, K, Yeval, n_splits, reduce_stat, random_state) 
-    #         for esti in estis)
-    mses = [cv_consecutive(esti, losses, X, Y, K, Yeval, n_splits, reduce_stat, random_state) for esti in estis]
+    with parallel_backend("loky"):
+        mses = Parallel(n_jobs=n_jobs)(
+            delayed(cv_consecutive)(esti, losses, X, Y, K, Yeval, n_splits, reduce_stat, random_state) 
+            for esti in estis)
+    # mses = [cv_consecutive(esti, losses, X, Y, K, Yeval, n_splits, reduce_stat, random_state) for esti in estis]
     print(mses)
     if isinstance(mses[0], torch.Tensor):
         mses = torch.stack(mses)
